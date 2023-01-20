26543e5db11e287018174085927444ad887e3d74
Covstar <okechukwuvictor093@gmail.com>
2023-01-20 20:26:16 +0100

    Finished countdown timer

---
+++ src/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+        // Set the date we're counting down to
+        var countDownDate = new Date("Jan 29, 2023 12:00:00").getTime(1674241130);
+
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+          // Get today's date and time
+          var now = new Date().getTime();
+
+          // Find the distance between now and the count down date
+          var distance = countDownDate - now;
+
+          // Time calculations for days, hours, minutes and seconds
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+          // Display the result in the element with id="days", "hours", "minutes", "seconds"
+          $("#days").html("0"+days);
+          $("#hours").html(hours);
+           $("#minutes").html(minutes);
+          $("#seconds").html(seconds);
+
+          
+          // If the count down is finished, write some text
+          if (distance < 0) {
+            clearInterval(x);
+            $("#days").html("0");
+            $("#hours").html("00");
+            $("#minutes").html("00");
+            $("#seconds").html("00");
+          }
+        }, 1000);
+
+      });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,108 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // backgroundImages
+      backgroundImage: {
+        'stars': "url('/images/bg-stars.svg')",
+        'mountains': "url('/images/pattern-hills.svg')",
+      },
+      // background size 
+      backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '100%': '100%',
+      '16': '4rem',
+    },
+
+    // letter spacing 
+    letterSpacing: {
+      widest: '.3em',
+    },
+
+    // media quries
+       screens: {
+      'sm': {'max': '720px'},
+      // => @media (max-width: 639px) { ... }
+      'tb': {'min': '721px', 'max': '1290px'},
+      // => @media (min-width: 768px and max-width: 1023px)
+      'md': {'min': '722px'},
+      // => @media (min-width: 768px) { ... }
+    },
+
+        // font family
+    fontFamily:{
+      'Red-Hat': ['Red Hat Text'],
+    },
+    
+    // padding 
+    padding: {
+        '3%': '3%',
+        '5%': '5%',
+        '7%': '7%',
+        '10%': '10%',
+        '15%': '15%',
+        '17%': '17%',
+        '20%': '20%',
+        '22%': '22%',
+        '27%': '27%',
+        '25%': '25%',
+        '24%': '24%',
+        '30%': '30%',
+        '35%': '35%',
+        '40%': '40%',
+        '50%': '50%',
+        '60%': '60%',
+      },
+       margin: {
+        '3%': '3%',
+        '5%': '5%',
+        '10%': '10%',
+        '15%': '15%',
+        '17%': '17%',
+        '20%': '20%',
+        '22%': '22%',
+        '27%': '27%',
+        '25%': '25%',
+        '24%': '24%',
+        '30%': '30%',
+        '40%': '40%',
+        '50%': '50%',
+        '60%': '60%',
+      },
+      width: {
+         '3%': '3%',
+        '5%': '5%',
+        '10%': '10%',
+        '15%': '15%',
+        '17%': '17%',
+        '20%': '20%',
+        '22%': '22%',
+        '27%': '27%',
+        '25%': '25%',
+        '24%': '24%',
+        '30%': '30%',
+        '40%': '40%',
+        '45%': '45%',
+        '50%': '50%',
+        '60%': '60%',
+        '70%': '70%',
+        '80%': '80%',
+        '90%': '90%',
+      },
+    // colors 
+     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'Grayish-blue': 'hsl(237, 18%, 59%)',
+       'soft-red': 'hsl(345, 95%, 68%)',
+       'Dark-desaturated-blue': 'hsl(236, 21%, 26%)',
+       'Very-dark-blue': 'hsl(235, 16%, 14%)',
+       'mostly-black': 'hsl(234, 17%, 12%)'
+    },
+    },
   },
   plugins: [],
 }
